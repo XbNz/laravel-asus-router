3741eb98f98443fbbd932a8986eebf286d86f025
Natachi <fdwnct@gmail.com>
2021-09-12 20:51:12 +0430

    remove vendor, composer lock from git

--- vendor/orchestra/testbench-core/laravel/config/router-config.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-return [
-    'router_username' => env('ROUTER_USER', 'ASUS'),
-    'router_ip_address' => env('ROUTER_IP', '192.168.50.1'),
-    'router_port' => env('ROUTER_PORT', '22'),
-    'timeout' => env('ROUTER_TIMEOUT', '1')
-];
